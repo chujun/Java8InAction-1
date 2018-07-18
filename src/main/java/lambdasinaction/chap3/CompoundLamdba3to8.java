5a086c31f7f1373dd8369486ee7f02d91735b01a
za-chujun <1571970439@qq.com>
2018-07-18 19:54:59 +0800

    modify

--- src/main/java/lambdasinaction/chap3/CompoundLamdba3to8.java
+++ src/main/java/lambdasinaction/chap3/CompoundLamdba3to8.java
@@ -11,11 +11,12 @@ import java.util.*;
 public class CompoundLamdba3to8 {
     public List<MyDate> getData() {
         List<MyDate> result = new ArrayList<>();
-        result.add(new MyDate(new Date()));
-        result.add(new MyDate(new Date(10000)));
+        result.add(new MyDate(new Date(), 1));
+        result.add(new MyDate(new Date(10000), 2));
         //result.add(new MyDate(null));
-        result.add(new MyDate(new Date(0)));
-        result.add(new MyDate(new Date(100)));
+        result.add(new MyDate(new Date(0), 3));
+        result.add(new MyDate(new Date(100), 4));
+        result.add(new MyDate(new Date(10), 4));
         return result;
     }
 
@@ -23,22 +24,34 @@ public class CompoundLamdba3to8 {
     public void testComparator() {
         List<MyDate> data = getData();
         //还是会抛出空指针异常的,如果内置currentDate为空的话
-        data.sort(Comparator.comparing(myDate -> myDate.getCurrentDate()));
+        data.sort(Comparator.comparing(MyDate::getCurrentDate));
         System.out.println(data);
+        data.sort(Comparator.comparing(MyDate::getIndex).thenComparing(MyDate::getCurrentDate));
+        System.out.println(data);
+
+        data.add(new MyDate());
+        //TODO:cj 怎么处理空指针的问题呢?
     }
 
     @Data
     static class MyDate {
-        public MyDate(Date currentDate) {
-            this.currentDate = currentDate;
+        public MyDate() {
         }
+
+        public MyDate(Date currentDate, int index) {
+            this.currentDate = currentDate;
+            this.index = index;
+        }
+
+        private int  index;
 
         private Date currentDate;
 
         @Override
         public String toString() {
             final StringBuilder sb = new StringBuilder("MyDate{");
-            sb.append("currentDate=").append(null != currentDate ? currentDate.getTime() : null);
+            sb.append("index=").append(index);
+            sb.append(", currentDate=").append(null == currentDate ? null : currentDate.getTime());
             sb.append('}');
             return sb.toString();
         }
