fa55659f7107d9e62395242be56c2955630fe77e
za-chujun <1571970439@qq.com>
2019-03-27 15:42:18 +0800

    reduce study

--- src/main/java/lambdasinaction/chap5/Reducing.java
+++ src/main/java/lambdasinaction/chap5/Reducing.java
@@ -1,31 +1,36 @@
 package lambdasinaction.chap5;
-import lambdasinaction.chap4.*;
 
-import java.util.stream.*;
-import java.util.*;
+import lambdasinaction.chap4.Dish;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import static lambdasinaction.chap4.Dish.menu;
 
-public class Reducing{
+public class Reducing {
 
-    public static void main(String...args){
+    public static void main(String... args) {
 
-        List<Integer> numbers = Arrays.asList(3,4,5,1,2);
+        List<Integer> numbers = Arrays.asList(3, 4, 5, 1, 2);
         int sum = numbers.stream().reduce(0, (a, b) -> a + b);
         System.out.println(sum);
 
         int sum2 = numbers.stream().reduce(0, Integer::sum);
         System.out.println(sum2);
 
-        int max = numbers.stream().reduce(0, (a, b) -> Integer.max(a, b));
+        int max = numbers.stream().reduce(0, Math::max);
         System.out.println(max);
 
-        Optional<Integer> min = numbers.stream().reduce(Integer::min);
+        Optional<Integer> min = numbers.stream().reduce(Math::min);
         min.ifPresent(System.out::println);
 
-        int calories = menu.stream()
-                           .map(Dish::getCalories)
-                           .reduce(0, Integer::sum);
+        int calories = menu.stream().map(Dish::getCalories).reduce(0, Integer::sum);
         System.out.println("Number of calories:" + calories);
+
+        int dishSize = menu.stream().map(d -> 1).reduce(0, Integer::sum);
+        System.out.println("dishSize:" + dishSize);
+        long count = menu.stream().count();
+        System.out.println(count);
     }
 }
