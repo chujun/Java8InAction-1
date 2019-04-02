d89d7451d5586916902fa23b668829f084ea771f
za-chujun <1571970439@qq.com>
2019-04-02 14:44:31 +0800

    add ToSetCollector

--- src/main/java/lambdasinaction/chap6/ToListCollector.java
+++ src/main/java/lambdasinaction/chap6/ToListCollector.java
@@ -9,7 +9,7 @@ public class ToListCollector<T> implements Collector<T, List<T>, List<T>> {
 
     @Override
     public Supplier<List<T>> supplier() {
-        return () -> new ArrayList<T>();
+        return ArrayList::new;
     }
 
     @Override
@@ -33,5 +33,47 @@ public class ToListCollector<T> implements Collector<T, List<T>, List<T>> {
     @Override
     public Set<Characteristics> characteristics() {
         return Collections.unmodifiableSet(EnumSet.of(IDENTITY_FINISH, CONCURRENT));
+    }
+
+    public static void main(String[] args) {
+        List<Dish> result = Dish.menu.stream().collect(new ToListCollector<Dish>());
+        System.out.println("use custom defined Collector interface:" + result);
+
+        Set<Dish> resultSet = Dish.menu.stream().collect(new ToSetCollector<Dish>());
+        System.out.println("use custom defined Collector interface,ToSetCollector:" + result);
+
+        result = Dish.menu.stream().collect(ArrayList::new, List::add, List::addAll);
+        System.out.println("use multi method" + result);
+    }
+
+    static class ToSetCollector<T> implements Collector<T, List<T>, Set<T>> {
+
+        @Override
+        public Supplier<List<T>> supplier() {
+            return ArrayList::new;
+        }
+
+        @Override
+        public BiConsumer<List<T>, T> accumulator() {
+            return (list, item) -> list.add(item);
+        }
+
+        @Override
+        public Function<List<T>, Set<T>> finisher() {
+            return i->new HashSet<>(i);
+        }
+
+        @Override
+        public Set<Characteristics> characteristics() {
+            return Collections.unmodifiableSet(EnumSet.of(IDENTITY_FINISH, CONCURRENT));
+        }
+
+        @Override
+        public BinaryOperator<List<T>> combiner() {
+            return (list1, list2) -> {
+                list1.addAll(list2);
+                return list1;
+            };
+        }
     }
 }
