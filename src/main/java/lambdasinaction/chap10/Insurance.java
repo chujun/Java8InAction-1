eed86c6eecdabe7c9bdf8a5d9993bd195de68a96
za-chujun <1571970439@qq.com>
2019-04-03 10:53:10 +0800

    OptionalMain

--- src/main/java/lambdasinaction/chap10/Insurance.java
+++ src/main/java/lambdasinaction/chap10/Insurance.java
@@ -1,5 +1,8 @@
 package lambdasinaction.chap10;
 
+import lombok.Data;
+
+@Data
 public class Insurance {
 
     private String name;
--- src/main/java/lambdasinaction/chap10/OptionalMain.java
+++ src/main/java/lambdasinaction/chap10/OptionalMain.java
@@ -15,9 +15,37 @@ public class OptionalMain {
         Optional<Person> person = Optional.of(new Person());
         person.get().setCar(optCar);
         System.out.println("多级null判断验证:" + getCarInsuranceName(person));
+        System.out.println(nullSafeFindCheapestInsurance(person, optCar));
+        System.out.println(nullSafeFindCheapestInsurance2(person, optCar));
+        System.out.println(nullSafeFindCheapestInsurance3(person, optCar));
     }
 
-    public static String getCarInsuranceName(Optional<Person> person) {
+    private static String getCarInsuranceName(Optional<Person> person) {
         return person.flatMap(Person::getCar).flatMap(Car::getInsurance).map(Insurance::getName).orElse("Unknown");
+    }
+
+    private static Optional<Insurance> nullSafeFindCheapestInsurance(Optional<Person> persion, Optional<Car> car) {
+        if (persion.isPresent() && car.isPresent()) {
+            return Optional.ofNullable(findCheapestInsurance(persion.get(), car.get()));
+        }
+        return Optional.empty();
+    }
+
+    private static Optional<Insurance> nullSafeFindCheapestInsurance2(Optional<Person> persion, Optional<Car> car) {
+        return persion.flatMap(p -> car.map(c -> findCheapestInsurance(p, c)));
+    }
+
+    private static Optional<Insurance> nullSafeFindCheapestInsurance3(Optional<Person> persion, Optional<Car> car) {
+        return persion.flatMap(p -> car.flatMap(c -> findCheapestInsuranceOptional(p, c)));
+    }
+
+    private static Insurance findCheapestInsurance(Person person, Car car) {
+        //不同的保险公司提供的查询服务
+        //对比所有数据
+        return new Insurance();
+    }
+
+    private static Optional<Insurance> findCheapestInsuranceOptional(Person person, Car car) {
+        return Optional.of(new Insurance());
     }
 }
